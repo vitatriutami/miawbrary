bba99d7d77e70bc7843b773eada836a3603978e2
vitatriutami <vitatriutami@gmail.com>
2024-06-04 13:33:25 +0700

    vite build

--- src/config/ApiUrl.ts
+++ src/config/ApiUrl.ts
@@ -1,4 +1,6 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "http://localhost:8000"
+    ? "http://103.74.5.102:8000"
     : "http://localhost:8000";
+
+// "http://103.74.5.102:8000";
